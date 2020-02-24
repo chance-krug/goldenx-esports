f45a2af2bdc04ae338b59d7577e9bbfe00a3badb
chance-krug <chance.krug@gmail.com>
2020-02-23 22:30:17 -0600

    Added beginning of Navigation Bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app-bar class="ma-0 pa-0" :collapse="!collapseOnScroll" color="blue-grey darken-4" dark fixed elevation="3" min-height="60">
+        <v-img
+                class="mx-2"
+                :src="require('@/assets/GXLogo.png')"
+                max-height="60"
+                max-width="60"
+                contain
+        />
+        <v-toolbar-title>Golden X Esports</v-toolbar-title>
+    </v-app-bar>
+</template>
+<script>
+    export default {
+        data: () => ({
+            collapseOnScroll: true,
+        }),
+    }
+
+</script>
+
+<style scoped>
+
+</style>
